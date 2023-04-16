58755888d934467f98e2ee110195e6e84d09c937
Eiler Poulsen <epo@klintra.fo>
2023-04-16 11:00:39 +0200

    Added db context, seed, and in memory db

--- Sample Web App/Sample Web App/Domain/Author.cs
+++ Sample Web App/Sample Web App/Domain/Author.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Sample_Web_App.Domain;
 
 public class Author
 {
+    [Key]
     public int AuthorId { get; set; }
 
     public string Name { get; set; }
