840761cf0fe467ab4a9e928801b7cdb504209771
Eiler Poulsen <epo@klintra.fo>
2023-04-17 09:56:11 +0200

    Refactored FetchAuthors from component class to component function

--- Sample Web App/Sample Web App/ClientApp/src/features/authors/fetchAuthors.js
+++ Sample Web App/Sample Web App/ClientApp/src/features/authors/fetchAuthors.js
@@ -1,53 +1,52 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 
-export class FetchAuthors extends Component {
-  static displayName = FetchAuthors.name;
+export function FetchAuthors() {
+  const [authors, setAuthors] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  constructor(props) {
-    super(props);
-    this.state = { authors: [], loading: true };
-  }
+  useEffect(() => {
+    populateAuthorsData();
+  }, []);
 
-  componentDidMount() {
-    this.populateAuthorsData();
-  }
-
-  static renderAuthorsTable(authors) {
+  function renderAuthorsTable(authors) {
     return (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
+        <table className="table table-striped" aria-labelledby="tableLabel">
+          <thead>
           <tr>
             <th>Name</th>
           </tr>
-        </thead>
-        <tbody>
-          {authors.map(author =>
-            <tr key={author.authorId}>
-              <td>{author.name}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+          {authors.map(author => (
+              <tr key={author.authorId}>
+                <td>{author.name}</td>
+              </tr>
+          ))}
+          </tbody>
+        </table>
     );
   }
 
-  render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchAuthors.renderAuthorsTable(this.state.authors);
+  async function populateAuthorsData() {
+    const response = await fetch('https://localhost:7289/api/Authors');
+    const data = await response.json();
+    setAuthors(data);
+    setLoading(false);
+  }
 
-    return (
+  let contents = loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+  ) : (
+      renderAuthorsTable(authors)
+  );
+
+  return (
       <div>
         <h1 id="tableLabel">Authors</h1>
         <p>This component demonstrates fetching data from the server.</p>
         {contents}
       </div>
-    );
-  }
-
-  async populateAuthorsData() {
-    const response = await fetch('https://localhost:7289/api/Authors');
-    const data = await response.json();
-    this.setState({ authors: data, loading: false });
-  }
+  );
 }
